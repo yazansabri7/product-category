b17ebfc1b1f9eae520f6c9d172958bc140245d32
yazansabri7 <sabriyazan2@gmail.com>
2024-10-23 13:16:09 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ displayCategories();
 
 
 const getProducts = async (page) => {
-    const skip =(page -1 )* 30 ;
+    const skip =(page -1 ) * 30 ;
     const {data} = await axios.get(`https://dummyjson.com/products?limit=30&skip=${skip}`);
     return data;
 }
@@ -43,11 +43,11 @@ const displayProducts = async (page =1 ) => {
     try{
     const products = await getProducts(page);
     const numberOfPages =  Math.ceil(products.total / 30);
-    console.log(page);
+    
     const result=products.products.map((product)=>{
         return `
             <div class="product">
-                <img src="${product.thumbnail}">
+                <img src="${product.thumbnail}" class="images">
                 <h3>${product.title}</h3>
                 <span>${product.price}$</span>
                 
@@ -83,6 +83,7 @@ const displayProducts = async (page =1 ) => {
 finally{
     loader.classList.remove("active");
 }
+        getmodal();
 }
 displayProducts();
 
@@ -117,3 +118,75 @@ const countDown = ( ) => {
 setInterval(() => {
    countDown();
 }, 1000);
+
+
+function getmodal() {
+    const modal = document.querySelector(".my-modal");
+    const closeBtn = document.querySelector(".close");
+    const leftBtn = document.querySelector(".left");
+    const rightBtn = document.querySelector(".right");
+    const images = Array.from(document.querySelectorAll(".images"));
+    let currentIndex = 0;
+    images.forEach(function(img){
+        
+
+    img.addEventListener("click",function(e){
+            modal.classList.remove("d-none");
+            modal.querySelector("img").setAttribute("src",e.target.src);
+            
+            currentIndex = images.indexOf(e.target)
+          
+        });
+   
+    })
+    //close modal
+    closeBtn.addEventListener("click",function(){
+        modal.classList.add("d-none");
+    })
+    rightBtn.addEventListener("click",function(){
+        currentIndex++;
+        if(currentIndex >= images.length){
+            currentIndex = 0;
+        }
+        const src = images[currentIndex].src;
+        modal.querySelector("img").setAttribute("src",src);
+    })
+
+    leftBtn.addEventListener("click",function(){
+        currentIndex--;
+        if(currentIndex<=0){
+            currentIndex = images.length - 1;
+
+        }
+        const src = images[currentIndex].src;
+        modal.querySelector("img").setAttribute("src",src);
+       
+    })
+
+    document.addEventListener("keydown",function(e){
+        
+        if(e.code=="ArrowRight"){
+            currentIndex++;
+        if(currentIndex >= images.length){
+            currentIndex = 0;
+        }
+        const src = images[currentIndex].src;
+        modal.querySelector("img").setAttribute("src",src);
+
+        }
+        if(e.code=="ArrowLeft"){
+            currentIndex--;
+        if(currentIndex<=0){
+            currentIndex = images.length - 1;
+
+        }
+        const src = images[currentIndex].src;
+        modal.querySelector("img").setAttribute("src",src);
+
+        }
+        if(e.code == "Escape"){
+            modal.classList.add("d-none");
+        }
+    })
+    
+}
